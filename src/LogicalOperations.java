4e611eb96c4ca667973ad65bda591ad3f435002b
ChristinaS <alexandra.christina@yahoo.com>
2022-06-26 00:22:10 +0300

    3 Java Homework

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -1,6 +1,5 @@
 public class LogicalOperations {
 
-    public int verifyTextAndNumber;
 
     public int getGreaterNumber(int first, int second) {
         if (first < second) {
@@ -11,9 +10,9 @@ public class LogicalOperations {
     }
 
     public String verifyTextAndNumber(String textInput, int number) {
-        if (textInput.equals("FastTrack") && number <= 3) {
+        if (textInput.equals("FastTrackIT") && number <= 3) {
             System.out.println(textInput + number);
-        } else if (!textInput.equals("FastTrack") && number >= 4) {
+        } else if (!textInput.equals("FastTrackIT") && number >= 4) {
             return (number + textInput);
         }
         return "Not applicable";
@@ -30,11 +29,71 @@ public class LogicalOperations {
     }
 
 
-    public int getNr(int nr) {
-        int nr;
-        if (nr <= 8 && nr < 2) {
-            return int nr;
+    public void getNr(int nr) {
+        if (nr >= 2 && nr <=8)
+            System.out.println(nr);
         }
-            return int nr;
+
+    public String vfNrgetText(int nr1){
+        if (nr1 > 8 || nr1 == 6){
+            System.out.println("The amount of snow this winter🎄 was:" + nr1 + "(cm)");
+        }else if (nr1<8 && nr1 != 6 && nr1 !=8){//am schimbat putin conditiile sa imi returneze ultima linie dar tot gol lasa! gresesc undeva?
+        System.out.println("The forecast snow is:" + nr1 + "(cm)");
+    }return "It will be sunny🌞!";}
+
+
+    public String vfdoublenr(double number1){
+        if (number1 >3 && number1 !=4){
+            System.out.println("The number1 is greater than 3 and not equal to 4.");
+        }else if (number1 == 4){
+            System.out.println("The number1 is equal to 4.");
+        } else if (number1 < 3) {
+            System.out.println("The number1 is lower than 3.");
+
+        }return "Not applicable";} //este interesant ca tot nu returneaza ultima linie cand nu sunt indeplinite conditiile
+
+
+
+       public void switchcase (int numar){
+
+               switch (numar){
+        case 100:
+            System.out.println("The number is:  100!"); break;
+        case 500:
+            System.out.println("The number is:  500!"); break;
+        case 1000:
+            System.out.println("The number is:  1000!"); break;
+        default:System.out.println("Not in 100, 500, 1000");
     }
-}
+    }
+    public boolean isNumberEven(int X){
+        if ((X%2)==0){
+            return true;
+                   }else {
+            return false;
+         }
+        }
+
+    public boolean isEligibleToVote(int Age){
+        if (Age>=18){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public String vfbiggestnumber(double n1, double n2, double n3){
+        if (n1 >n2 && n1>=n3){
+            System.out.println("The number n1 is the biggest:" + n1);
+        }else if (n2 >n1 && n2>=n3){
+            System.out.println("The number n2 is the biggest:" + n2);
+        } else if (n3 >n2 && n3>=n1) {
+            System.out.println("The number n3 is the biggest:" + n3);
+
+        }return "Not applicable";}
+
+
+    }
+
+
+
+
--- src/Main3.java
+++ src/Main3.java
@@ -19,7 +19,7 @@ public class Main3 {
 // print the number and the text, in this order.
 
 
-   String textInput = "FastTrack";
+   String textInput = "FastTrackIT";
    int number = 4;
         System.out.println(op.verifyTextAndNumber(textInput,number));
 
@@ -31,13 +31,78 @@ public class Main3 {
 // 3. Given a number, if it’s equal to or higher than 2 and
 // equal to or lower than 8, print the number
 
-        System.out.println(op.getNr());
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter nr: ");
         int nr = scanner.nextInt();
+        op.getNr(nr);
+
+//6. Creati o metoda de tip String, care sa primeasca
+// un parametru de tip int. Daca numarul primit este
+// mai mare de 8, SAU egal cu 6, atunci sa se returneze
+// “The amount of snow this winter was(cm): “ si numarul.
+// Daca nu, sa printeze “The forecast snow is(cm):”
+// si numarul. Apelati metoda in main() pentru a verifica
+// daca functioneaza.
+
+        System.out.println("Please enter nr1: ");
+        int nr1 = scanner.nextInt();
+        op.vfNrgetText(nr1);
+
+//7. Creati o metoda de tip String, care sa primeasca un parametru de tip numar.
+// Daca numarul e mai mare decat 3, SI nu egal cu 4, returnati “The number is greater than 3 and not equal to 4”.
+// Daca numarul este egal cu 4, returnati “The number is equal to 4”,
+// iar daca numarul este mai mic de 3 returnati “The number is lower than 3”.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+//
+        System.out.println("Please enter number1: ");
+        double number1 = scanner.nextDouble();
+        op.vfdoublenr(number1);
 
 
+//8. Creati o metoda care primeste un numar ca si parametru si sa afiseze ce numar a primit.
+// Folosind constructia Switch-Case, verificati ce numar s-a primit si afisati textul "The number is:  x !"
+// unde x trebuie sa reprezinte numarul apasat.
+// Pentru exemplul de fata sa nu se foloseasca concatenarea stringului
+// "The number is:" cu numarul de la parametru, ci sa se scrie intreg textul
+// cu tot cu numar pentru fiecare caz din Switch-Case.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+//
+        System.out.println("Select one number:");
+      int numar = scanner.nextInt();
+      op.switchcase(numar);
 
 
+//9. Creati o metoda care sa se numeasca isNumberEven. Metoda trebuie sa primeasca ca parametru un numar,
+// si sa returneze adevarat sau fals. Daca numarul e par sa returneze true iar daca e impar sa returnese false.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+//
+        System.out.println("Select X:");
+        int X = scanner.nextInt();
+        op.isNumberEven(X);
+        System.out.println(op.isNumberEven(X));
+
+
+//10. Creati o metoda care sa se numeasca isEligibleToVote. Metoda sa primeasca ca si parametru un numar,
+// care sa reprezinte varsta, iar metoda sa iti zica daca daca persoana are drept de vot.
+// Daca varsta e peste 18, sa se returneze true iar daca nu, sa returneze false.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+//
+        System.out.println("Select Age to vote:");
+        int Age = scanner.nextInt();
+        op.isEligibleToVote(Age);
+        System.out.println(op.isEligibleToVote(Age));
+
+
+//11. Creati o metoda care sa primeasca 3 numere ca si parametrii si sa returneze cel mai mare numar.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+
+        System.out.println("Please enter n1: ");
+        double n1 = scanner.nextDouble();
+        System.out.println("Please enter n2: ");
+        double n2 = scanner.nextDouble();
+        System.out.println("Please enter n3: ");
+        double n3 = scanner.nextDouble();
+        op.vfbiggestnumber(n1, n2, n3);
     }
 }
